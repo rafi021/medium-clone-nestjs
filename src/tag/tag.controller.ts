2a04346f0d9ec3e9e2bdc10755affe97c5caeae1
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-04-09 14:04:46 +0600

    created module,controller and service

---
+++ src/tag/tag.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { TagService } from './tag.service';
+
+@Controller('tags')
+export class TagController {
+    constructor(private readonly tagsService: TagService){}
+    @Get()
+    findAll(){
+        return this.tagsService.findAll();
+    }
+}
